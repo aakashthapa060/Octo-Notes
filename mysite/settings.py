24ed994ef0c0f19a57a5f0f80843f37b10f18d62
Aakash Thapa <aakashmanm3@gmail.com>
2021-09-02 17:10:13 +0545

    Setting Changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
